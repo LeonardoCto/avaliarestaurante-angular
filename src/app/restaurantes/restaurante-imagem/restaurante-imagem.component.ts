dd66f4c9fa1afcfc539e424bb13109557a2b9910
Victor Saccucci <victorsaccucciv@gmail.com>
2023-11-10 19:07:28 -0300

    Fluxo de cadastro imagem

--- src/app/restaurantes/restaurante-imagem/restaurante-imagem.component.ts
+++ src/app/restaurantes/restaurante-imagem/restaurante-imagem.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
-
+import { Router
+ } from '@angular/router';
 @Component({
   selector: 'app-restaurante-imagem',
   templateUrl: './restaurante-imagem.component.html',
   styleUrls: ['./restaurante-imagem.component.scss']
 })
 export class RestauranteImagemComponent {
+
+  constructor(private router: Router) { }
+
+  avancar() {
+
+    this.router.navigate(['/imagem']);
+  }
 
 }
